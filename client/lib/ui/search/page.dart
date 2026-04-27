2317d820c140aba8aef361270beb15164c4b9440
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2026-04-27 15:38:10 +0700

    fix(client): show sheet when cluster clicked

--- client/lib/ui/search/page.dart
+++ client/lib/ui/search/page.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:openrent_client/data/remote/product.dart';
 import 'package:openrent_client/ui/components/product_card.dart';
 import 'package:openrent_client/ui/search/cubit.dart';
 import 'package:openrent_client/ui/search/state.dart';
@@ -65,89 +68,119 @@ class _SearchResult extends StatelessWidget {
                   ProductCard(item: state.result.elementAt(index)),
               itemCount: state.result.length,
             )
-          : FlutterMap(
-              options: MapOptions(
-                initialCenter: LatLng(-7.771720, 110.412984),
-                initialZoom: 9.2,
+          : _SearchResultMap(
+              list: state.result,
+              onClusterClicked: (list) => showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                // TODO: Make it draggable
+                builder: (context) => ListView.builder(
+                  itemBuilder: (context, index) =>
+                      ProductCard(item: state.result.elementAt(index)),
+                  itemCount: state.result.length,
+                ),
               ),
-              children: [
-                TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName:
-                      'OpenRent/0.1 (contact: levirs565@gmail.com)',
-                ),
-                MarkerClusterLayerWidget(
-                  options: MarkerClusterLayerOptions(
-                    maxClusterRadius: 45,
-                    size: const Size(40, 40),
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(50),
-                    maxZoom: 15,
-                    spiderfyCluster: false,
-                    onClusterTap: (cluster) {
-                      showBottomSheet(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              const Text('Bottom sheet'),
-                              ElevatedButton(
-                                child: const Text('Close'),
-                                onPressed: () => Navigator.pop(context),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    markers: state.result
-                        .map(
-                          (item) => Marker(
-                            alignment: Alignment.topCenter,
-                            point: LatLng(item.address.lat, item.address.lng),
-                            width: 48,
-                            height: 48,
-                            child: GestureDetector(
-                              onTap:  () => Navigator.of(context).push(ProductDetailPage.route(item.id)),
-                              child: Icon(
-                                Icons.location_on,
-                                size: 48,
-                                color: Theme.of(context).colorScheme.primary,
-                              ),
-                            ),
-                          ),
-                        )
-                        .toList(),
-                    builder: (context, markers) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blue,
-                        ),
-                        child: Center(
-                          child: Text(
-                            markers.length.toString(),
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                RichAttributionWidget(
-                  attributions: [
-                    TextSourceAttribution(
-                      'OpenStreetMap contributors',
-                      onTap: () => launchUrl(
-                        Uri.parse('https://openstreetmap.org/copyright'),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
             ),
+    );
+  }
+}
+
+class _SearchResultMap extends StatelessWidget {
+  final List<ProductResponseItemShort> list;
+  final void Function(List<ProductResponseItemShort> items) onClusterClicked;
+
+  const _SearchResultMap({
+    super.key,
+    required this.list,
+    required this.onClusterClicked,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: LatLng(-7.771720, 110.412984),
+        initialZoom: 9.2,
+        maxZoom: 20,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'OpenRent/0.1 (contact: levirs565@gmail.com)',
+        ),
+        MarkerClusterLayerWidget(
+          options: MarkerClusterLayerOptions(
+            maxClusterRadius: 45,
+            size: const Size(40, 40),
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(50),
+            maxZoom: 20,
+            disableClusteringAtZoom: 100,
+            spiderfyCluster: false,
+            onClusterTap: (cluster) {
+              onClusterClicked(
+                cluster.markers
+                    .map(
+                      (marker) => list.firstWhere(
+                        (item) =>
+                            item.id == (marker.key as ValueKey<int>).value,
+                      ),
+                    )
+                    .toList(),
+              );
+            },
+            markers: list
+                .map(
+                  (item) => Marker(
+                    key: ValueKey(item.id),
+                    alignment: Alignment.center,
+                    point: LatLng(item.address.lat, item.address.lng),
+                    width: 40,
+                    height: 40,
+                    child: GestureDetector(
+                      onTap: () => Navigator.of(
+                        context,
+                      ).push(ProductDetailPage.route(item.id)),
+                      child: _MapItem(text: "1"),
+                    ),
+                  ),
+                )
+                .toList(),
+            builder: (context, markers) =>
+                _MapItem(text: markers.length.toString()),
+          ),
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () =>
+                  launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _MapItem extends StatelessWidget {
+  final String text;
+
+  const _MapItem({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.blue,
+      ),
+      child: Center(
+        child: Text(text, style: const TextStyle(color: Colors.white)),
+      ),
     );
   }
 }
